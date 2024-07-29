4e4ab6a37e80203a00fd6fe91da883d7f4307af1
Gokul <gokulgk9402@gmail.com>
2024-07-29 16:31:24 +0530

    added base ui code for reports page

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,9 +1,12 @@
+import { ReportsForm } from "@/components/ReportsForm";
 import React from "react";
 
 const ReportsPage = () => {
   return (
     <main className="container ">
-      <div className=" mx-auto my-4 max-w-screen-lg border-l border-r border-border"></div>
+      <div className=" mx-auto my-4 max-w-screen-lg border-l border-r border-border">
+        <ReportsForm />
+      </div>
     </main>
   );
 };
