a3c832649da25967ee04c230394617342e509956
Gokul <gokulgk9402@gmail.com>
2024-07-29 14:46:23 +0530

    update lint rule prefer-const to warn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,7 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   rules: {
+    "prefer-const": "warn",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
