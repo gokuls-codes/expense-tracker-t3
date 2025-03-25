40b28bf7b976f0cd4b7c66f32ada0e742c17cf3a
Gokul <gokulgk9402@gmail.com>
2025-03-25 17:08:16 +0530

    push webhook from upstox to db to test

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,11 +1,19 @@
+import { db } from "@/server/db";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  return NextResponse.json({ message: "Hello from GET /api/orders" });
+  const resp = await db.testStock.findMany();
+  return NextResponse.json(resp);
 }
 
 export async function POST(request: NextRequest) {
-  const requestBody: unknown = await request.json();
+  const requestBody: object = await request.json();
+  // console.log(typeof requestBody);
+  await db.testStock.create({
+    data: {
+      payload: requestBody,
+    },
+  });
   console.log(requestBody);
   return NextResponse.json({ message: "Hello from POST /api/orders" });
 }
