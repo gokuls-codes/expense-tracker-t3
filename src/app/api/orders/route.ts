cf22bb39bff7c26ff2f6cf556391b1a970fdcd20
Gokul <gokulgk9402@gmail.com>
2025-03-25 09:42:41 +0530

    added code to console log request from upstox webhook

---
+++ src/app/api/orders/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  return NextResponse.json({ message: "Hello from GET /api/orders" });
+}
+
+export async function POST(request: NextRequest) {
+  const requestBody = await request.json();
+  console.log(requestBody);
+  return NextResponse.json({ message: "Hello from POST /api/orders" });
+}
